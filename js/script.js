35bb9acc5a484df61411e5e3de596a9f121f6540
esraa237 <esraafoda2000@gmail.com>
2024-05-11 03:38:33 +0300

    add main js file

--- js/script.js
+++ js/script.js
@@ -4,68 +4,55 @@ const nav = document.querySelector(".nav"),
   navOpenBtn = document.querySelector(".navOpenBtn"),
   navCloseBtn = document.querySelector(".navCloseBtn"),
   linksOpenBtn = document.querySelector("#open-icon"),
-  userBox = document.querySelector(".user-box"),
-  //variables for confirmed password
-  password = document.getElementById("password2"),
-  confirm_password = document.getElementById("confirm_password");
-  //variables for profile page
-  let profileImg=document.querySelector(".account-details .image-div img"),
-  userFile=document.querySelector(".account-details .userfile");
-  let token = 1;//token=1 if submitting or login
-  let userIconClick = 0;
-  
-  //functions for nav bar
-  searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if (nav.classList.contains("openSearch")) {
-      return searchIcon.classList.replace("fa-magnifying-glass", "fa-xmark");
-    }
-    searchIcon.classList.replace("fa-xmark","fa-magnifying-glass");
-  });
-  
-  navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("fa-xmark","fa-magnifying-glass");
-  });
-  navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
-  });
+  userBox = document.querySelector(".user-box");
 
-linksOpenBtn.addEventListener("click", () => {
-    if(token==1&&userIconClick==0){
-      userBox.classList.add("showAfter");
-      userIconClick=1;
-      linksOpenBtn.classList.replace("fa-user","fa-xmark");
-    }
-    else if(userIconClick==0&&token==0){
-      userBox.classList.add("showBefore");
-      userIconClick=1;
-      linksOpenBtn.classList.replace("fa-user","fa-xmark");
-    }
-    else if(userIconClick=1){
-      userBox.classList.remove("showBefore");
-      userBox.classList.remove("showAfter");
-      userIconClick=0;
-      linksOpenBtn.classList.replace("fa-xmark","fa-user");
-    }
+
+//variables for profile page
+let profileImg = document.querySelector(".account-details .image-div img"),
+  userFile = document.querySelector(".account-details .userfile");
+let token=0;
+//export {token};//token=1 if submitting or login
+let userIconClick = 0;
+
+//functions for nav bar
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("fa-magnifying-glass", "fa-xmark");
+  }
+  searchIcon.classList.replace("fa-xmark", "fa-magnifying-glass");
 });
 
-//functions for confirmed password
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("fa-xmark", "fa-magnifying-glass");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
+
+linksOpenBtn.addEventListener("click", () => {
+  if (token==1 && userIconClick == 0) {
+    userBox.classList.add("showAfter");
+    userIconClick = 1;
+    linksOpenBtn.classList.replace("fa-user", "fa-xmark");
+  } else if (userIconClick == 0 && token == 0) {
+    userBox.classList.add("showBefore");
+    userIconClick = 1;
+    linksOpenBtn.classList.replace("fa-user", "fa-xmark");
+  } else if ((userIconClick = 1)) {
+    userBox.classList.remove("showBefore");
+    userBox.classList.remove("showAfter");
+    userIconClick = 0;
+    linksOpenBtn.classList.replace("fa-xmark", "fa-user");
   }
-}
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+});
 
 //Profile picture
-userFile.onchange=function(){
-  profileImg.src=URL.createObjectURL(userFile.files[0]);
-}
+userFile.onchange = function () {
+  profileImg.src = URL.createObjectURL(userFile.files[0]);
+};
 

3eedf82863a35f3d56c813347d847e1bd5148889
esraa237 <esraafoda2000@gmail.com>
2024-05-11 03:39:05 +0300

    add signup js file

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,5 @@
-const signupCloseBtn = document.querySelector(".signup-close"),
+
+let signupCloseBtn = document.querySelector(".signup-close"),
   //variables for signup multi levels
   slidePage = document.querySelector(".slide-page"),
   nextBtnFirst = document.querySelector(".firstNext"),
@@ -9,15 +10,13 @@ const signupCloseBtn = document.querySelector(".signup-close"),
   submitBtn = document.querySelector(".Sign-Up"),
   progressCheck = document.querySelectorAll(".step .check"),
   bullet = document.querySelectorAll(".step .bullet");
-let current = 1;
+  let current = 1;
 
-//variables for confirmed password
-const password = document.getElementById("password2"),
-  confirm_password = document.getElementById("confirm_password");
 
 signupCloseBtn.addEventListener(
-  "click",
-  () => (window.location.href = "index.html")
+  "click",() =>{
+    window.location.href = "index.html";
+  }
 );
 //functions for signup multi levels
 submitBtn.addEventListener("click", function () {
@@ -25,13 +24,13 @@ submitBtn.addEventListener("click", function () {
   progressCheck[current - 1].classList.add("active1");
   current += 1;
   setTimeout(function () {
-    alert("Your Form Successfully Signed up");
-    location.reload();
-  }, 800);
+    window.location.href="index.html";
+  }, 300);
+  
 });
 nextBtnFirst.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-36.5%";
+  slidePage.style.marginLeft = "-35.9%";
   bullet[current - 1].classList.add("active1");
   progressCheck[current - 1].classList.add("active1");
   current += 1;
@@ -39,7 +38,7 @@ nextBtnFirst.addEventListener("click", function (event) {
 
 nextBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-72.5%";
+  slidePage.style.marginLeft = "-71.1%";
   bullet[current - 1].classList.add("active1");
   progressCheck[current - 1].classList.add("active1");
   current += 1;
@@ -54,19 +53,8 @@ prevBtnSec.addEventListener("click", function (event) {
 });
 prevBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
-  slidePage.style.marginLeft = "-36.5%";
+  slidePage.style.marginLeft = "-35.9%";
   bullet[current - 2].classList.remove("active1");
   progressCheck[current - 2].classList.remove("active1");
   current -= 1;
 });
-//functions for confirmed password
-
-function validatePassword() {
-  if (password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity("");
-  }
-}
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
